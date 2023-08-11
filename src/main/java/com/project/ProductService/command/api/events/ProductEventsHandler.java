ab03d3e70da5747dc390240551e8bccd8e016fbe
isml26 <iguven1999@hotmail.com>
2023-08-11 23:52:06 +0300

    command

---
+++ src/main/java/com/project/ProductService/command/api/events/ProductEventsHandler.java
@@ -0,0 +1,33 @@
+package com.project.ProductService.command.api.events;
+
+import com.project.ProductService.command.api.data.Product;
+import com.project.ProductService.command.api.data.ProductRepository;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@ProcessingGroup("product")
+public class ProductEventsHandler {
+
+    private ProductRepository productRepository;
+
+    public ProductEventsHandler(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @EventHandler
+    public void on(ProductCreatedEvent event) {
+        Product product =
+                new Product();
+        BeanUtils.copyProperties(event,product);
+        productRepository.save(product);
+    }
+
+    @ExceptionHandler
+    public void handle(Exception exception) throws Exception {
+        throw exception;
+    }
+}
